686ec24be521c27de747f5296dc11f2c5e1fc1d2
gonchigg <garciagentag@gmail.com>
2020-05-26 11:38:19 -0300

    remove tests from repo

--- tests/tests_01.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-a = [ [ np.array([1,2,3]), np.array([2,3,4]), np.array([4,5,6]) ],
-      [ np.array([7,8,9]), np.array([2,3,4]), np.array([4,5,6]) ],
-      [ np.array([1,2,4]), np.array([2,4,6]), np.array([8,7,5]) ] ]
-
-print(a[0])
